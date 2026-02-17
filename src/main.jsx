7e8fc020a3aa1ca227ebf734645f39c35e2df729
sushmitabhelkar24 <sushmita202301@gmail.com>
2026-02-17 22:56:28 +0530

    react component

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Greeting from './Greeting'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <Greeting/>
+  </StrictMode>,
+)
